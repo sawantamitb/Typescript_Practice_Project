92491400f14d548bdfb3c9aed82a501833248072
Amit Sawant <sawantamitb@gmail.com>
2026-06-13 09:30:49 +0530

    variaable file changes

---
+++ variable.ts
@@ -0,0 +1,25 @@
+var myname:string= "Amit";
+let myage:number= 35;
+const mycity:string= "New York";
+
+console.log("My name is " + myname+ ", I am " + myage + " years old and I live in " + mycity + ".");   
+
+myname = "Deepu";
+myage = 30;
+//mycity = "Mumbai";
+
+console.log("My name is " + myname+ ", I am " + myage + " years old and I live in " + mycity + "."); 
+
+var myname = "Darshan";
+//let myage = 25;
+//const mycity = "Delhi";
+
+console.log("My name is " + myname+ ", I am " + myage + " years old and I live in " + mycity + "."); 
+
+{
+    myname = "Ravi";
+    myage = 15;
+    //mycity = "Surat";
+}
+
+console.log("My name is " + myname+ ", I am " + myage + " years old and I live in " + mycity + "."); 
